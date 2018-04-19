7de1a89f57c7300a04d1b1422e76994460f4efb7
Josh Dobson <joshua@joshuarule.com>
2018-04-19 12:47:51 -0400

    adds FielMetaData to changeObect call

--- src/Control.js
+++ src/Control.js
@@ -1,34 +1,65 @@
-import PropTypes from 'prop-types';
-import React from 'react';
+import PropTypes from "prop-types";
+import React, { Fragment } from "react";
 
 export default class Control extends React.Component {
   static propTypes = {
     onChange: PropTypes.func.isRequired,
     forID: PropTypes.string,
     value: PropTypes.node,
-    classNameWrapper: PropTypes.string.isRequired,
-  }
+    classNameWrapper: PropTypes.string.isRequired
+  };
 
   static defaultProps = {
-    value: '',
+    value: new Map()
+  };
+
+  onChangeObject(field, val) {
+    console.log(field);
+    console.log(val);
   }
 
   render() {
     const {
       forID,
+      field,
       value,
       onChange,
+      onChangeObject,
       classNameWrapper,
+      metadata
     } = this.props;
 
+    console.log("value", value);
+    // console.log("props", this.props);
+
     return (
-      <input
-        type="text"
-        id={forID}
-        className={classNameWrapper}
-        value={value || ''}
-        onChange={e => onChange(e.target.value)}
-      />
+      <div className={"test"}>
+        <input
+          type="text"
+          id="title"
+          field="title"
+          className={classNameWrapper}
+          value={value.get("title") || ""}
+          onChange={val =>
+            onChangeObject("title", val.target.value, {
+              title: val.target.value
+            })
+          }
+        />
+
+        <input
+          type="text"
+          id="alt"
+          field="alt"
+          className={classNameWrapper}
+          value={value.get("alt") || ""}
+          onChange={val =>
+            onChangeObject("alt", val.target.value, {
+              alt: val.target.value
+            })
+          }
+        />
+      </div>
     );
   }
 }
--- src/Preview.js
+++ src/Preview.js
@@ -1,10 +1,16 @@
-import PropTypes from 'prop-types';
-import React from 'react';
+import PropTypes from "prop-types";
+import React from "react";
 
-export default function Preview({ value }) {
-  return <div>{ value }</div>;
+export default function Preview(props) {
+  console.log(props);
+  return (
+    <div>
+      <p>{props.fieldsMetaData.get("title")}</p>
+      <p>{props.fieldsMetaData.get("alt")}</p>
+    </div>
+  );
 }
 
 Preview.propTypes = {
-  value: PropTypes.node,
+  value: PropTypes.node
 };
